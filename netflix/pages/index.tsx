266aa67381b1c1940c7da2cad9368bd64571af59
T G SHIV KUMAR <tg2orshiv@gmail.com>
2025-02-21 15:22:27 +0530

    protected routes, made profile.

--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,8 +1,24 @@
-
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import UseCurrentUser from '@/hooks/useCurrentUser';
+import Navbar from "./navbar";
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
 export default function Home() {
+  const { data:user } = UseCurrentUser();
   return (
-    <>
-      <h1 className="text-5xl text-red-400">Netflix clone</h1>
-    </>
+    <Navbar/>
   );
 }
---
+++ netflix/pages/navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full fixed z-40">
+            <div className="
+                px-4
+                md:px-16
+                py-6
+                flex
+                flex-row
+                items-center
+                transition
+                duration-400
+                bg-zinc-900
+                bg-opacity-90            
+            ">
+                <img className="h-20 lg:h-30" src="/images/logo.png" alt="" />
+                <div className="
+                    
+                "></div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
